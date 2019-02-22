79c288bfa0cbad6cf32cd6aaa8811aa10a84931b
Marcus Vinicius Silva Costa <Marcus_V_Costa@DellTeam.com>
2019-02-22 17:19:15 -0300

    Adding Area Help, to documentation of webAPI update fonts reference folder location

---
+++ Vote/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Vote.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Vote/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vote.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
